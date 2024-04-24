bf9153cbdd38681079db03ce1922edf1df9cf04f
MACC <cesar@studiosupermassive.com>
2024-04-23 21:42:59 -0700

    Linting error

--- src/app/courses/components/render-courses.component.tsx
+++ src/app/courses/components/render-courses.component.tsx
@@ -5,7 +5,7 @@ import { BrainstormContext } from "@/app/context/context-provider";
 const RenderCourses = ({ data }: any) => {
   const ctx = useContext(BrainstormContext);
   console.log('BrainstormContext', ctx);
-  
+
   return (
     <div>
       <h1>Current Courses</h1>
