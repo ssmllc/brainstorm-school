9d0644d4dc6f37654de8b16800cb01d2b268ac96
MACC <cesar@studiosupermassive.com>
2024-04-23 21:53:06 -0700

    dont use try catch on fetch

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -9,20 +9,14 @@ export const metadata: Metadata = {
 const Courses = async () => {
   let data: any = [];
 
-  try {
-    const query = encodeURI('https://y8rjsgga.api.sanity.io/v2024-04-09/data/query/production?query=*[_type=="courses"] {title, start, instructor, mainImage {asset -> {url}}, courses[] -> {title, description}, body[0] { children[0] {text}}}');
-    const response = await fetch(query, { cache: 'no-store' });
-    
-    if (!response.ok) {
-      throw new Error(`HTTP error: Status ${response.status}`);
-    }
-
-    data = await response.json();
-  } catch (err: any) {
-    console.log("error", err);
-  } finally {
-    // setLoading(false);
+  const query = encodeURI('https://y8rjsgga.api.sanity.io/v2024-04-09/data/query/production?query=*[_type=="courses"] {title, start, instructor, mainImage {asset -> {url}}, courses[] -> {title, description}, body[0] { children[0] {text}}}');
+  const response = await fetch(query, { cache: 'no-store' });
+  
+  if (!response.ok) {
+    throw new Error(`HTTP error: Status ${response.status}`);
   }
+
+  data = await response.json();
 
   return(
     <div>
