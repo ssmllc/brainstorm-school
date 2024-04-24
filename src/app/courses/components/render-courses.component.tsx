56d6be3417b5a2f9928758db0ddfbfa3aa40469f
MACC <cesar@studiosupermassive.com>
2024-04-23 21:49:17 -0700

    mapping issue

--- src/app/courses/components/render-courses.component.tsx
+++ src/app/courses/components/render-courses.component.tsx
@@ -2,14 +2,15 @@
 
 import { useContext } from "react";
 import { BrainstormContext } from "@/app/context/context-provider";
-const RenderCourses = ({ data }: any) => {
+
+const RenderCourses = ({ data = [] }: any) => {
   const ctx = useContext(BrainstormContext);
   console.log('BrainstormContext', ctx);
 
   return (
     <div>
       <h1>Current Courses</h1>
-      {data.result.map((course:any, index:number) => (
+      {data && data.result.map((course:any, index:number) => (
         <p key={index}>{ course.title }</p>
       ))}
     </div>
