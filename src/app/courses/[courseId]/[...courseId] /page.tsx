2667a888c643f6b65bc4f815dd9a3105f77363f6
MACC <cesar@studiosupermassive.com>
2024-05-20 16:34:33 -0700

    Update query

--- src/app/courses/[courseId]/[...courseId] /page.tsx
+++ src/app/courses/[courseId]/[...courseId] /page.tsx
@@ -9,7 +9,7 @@ import RenderCourses from "../../components/render-courses.component";
 
 const fetchData = async () => {
   const query =
-    "https://y8rjsgga.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27course%27%5D+%7B%0A++name%0A%7D";
+    "https://y8rjsgga.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27courses%27%5D+%7B%0A++category%2C%0A++slug+%7B%0A++++current%0A++%7D%2C%0A++details%5B0%5D+%7B%0A++++children%5B0%5D+%7B%0A++++++text%0A++++%7D%0A++%7D%2C%0A++sections%5B%5D+-%3E+%7B%0A++++section%2C%0A++++category%2C%0A++++slug%2C%0A++++courses%5B%5D+-%3E+%7B%0A++++++name%2C%0A++++++section%2C%0A++++++category%2C%0A++++++slug%2C%0A++++++code%2C%0A++++++description%5B0%5D+%7B%0A++++++++children%5B0%5D+%7B%0A++++++++++text%0A++++++++%7D%0A++++++%7D%2C%0A++++++time%2C%0A++++++duration%2C%0A++++++preview+%7B%0A++++++++asset+-%3E+%7B%0A++++++++++url%0A++++++++%7D%2C%0A++++++%7D%2C%0A++++++schedule%5B%5D+-%3E+%7B%0A++++++++title%2C%0A++++++++profession%2C%0A++++++++bio%2C%0A++++++++registration%2C%0A++++++++term%2C%0A++++++++time%2C%0A++++++++start%2C%0A++++++++duration%2C%0A++++++%7D%2C%0A++++%7D%2C%0A++%7D%2C%0A%7D";
   const response = await fetch(query, { cache: "no-store" });
 
   if (!response.ok) {
@@ -28,7 +28,7 @@ export default async function Page({
 }) {
   const result = await fetchData();
   console.log("base result", result);
-  console.log("courseId", params.courseId);
+  console.log("courseId2", params.courseId);
 
   // const selectedCourses = result.filter(
   //   (res: any) => res.slug.current === params.courseId
