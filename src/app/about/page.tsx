595faeaef399c4ad95a3e1722e77405199f63667
MACC <cesar@studiosupermassive.com>
2024-05-27 19:41:40 -0700

    Mini cards layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,13 +7,33 @@ import RegistrationBlock from "../components/registration/registration-block.com
 import VideoContentSplit from "../components/registration/video-content-split.component";
 import { Card } from "../components/card/card-card.component";
 import TextHeaderBlock from "../components/text-block/text-header-block.component";
+import ImageGallery from "../components/gallery/image-gallery.component";
+import FAQs from "../components/text-block/faqs.component";
+import { MiniCards } from "../components/card/mini-card.component";
 
 export const metadata: Metadata = {
   title: "Brainstorm Courses - Brainstorm School",
   description: "Courses for brainstorm school",
 };
 
+const fetchData = async () => {
+  const query =
+    "https://y8rjsgga.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27courses%27%5D+%7B%0A++category%2C%0A++slug+%7B%0A++++current%0A++%7D%2C%0A++details%5B0%5D+%7B%0A++++children%5B0%5D+%7B%0A++++++text%0A++++%7D%0A++%7D%2C%0A++faqs%5B%5D+-%3E+%7B%0A++++question%2C%0A++++answer%2C%0A++%7D%2C%0A++sections%5B%5D+-%3E+%7B%0A++++section%2C%0A++++category%2C%0A++++slug%2C%0A++++courses%5B%5D+-%3E+%7B%0A++++++_id%2C%0A++++++name%2C%0A++++++section%2C%0A++++++category%2C%0A++++++slug%2C%0A++++++code%2C%0A++++++description%5B0%5D+%7B%0A++++++++children%5B0%5D+%7B%0A++++++++++text%0A++++++++%7D%0A++++++%7D%2C%0A++++++preview+%7B%0A++++++++asset+-%3E+%7B%0A++++++++++url%0A++++++++%7D%2C%0A++++++%7D%2C%0A++++++instructors%5B%5D+-%3E+%7B%0A++++++++title%2C%0A++++++++profession%2C%0A++++++++bio%2C%0A++++++%7D%2C%0A++++++schedule%5B%5D+-%3E+%7B%0A++++++++instructor%5B0%5D+-%3E+%7B%0A++++++++++title%2C%0A++++++++%7D%2C%0A++++++++registration%2C%0A++++++++term%2C%0A++++++++time%2C%0A++++++++duration%2C%0A++++++++start%2C%0A++++++%7D%2C%0A++++%7D%2C%0A++%7D%2C%0A%7D";
+  // const response = await fetch(query);
+  const response = await fetch(query, { cache: "no-store" });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error: Status ${response.status}`);
+  }
+
+  const { result } = await response.json();
+
+  return result;
+};
+
 export default async function About() {
+  const result = await fetchData();
+
   return (
     <Container margin="100px 0 0 0">
       {/* <MediaBanner
@@ -106,6 +126,19 @@ export default async function About() {
           </TextHeaderBlock>
         </FlexContainer>
       </FlexContainer>
+
+      <FlexContainer margin="0 auto">
+        <Container>
+          <div></div>
+          <div></div>
+        </Container>
+      </FlexContainer>
+
+      <MiniCards header="Spring 2024" width="70%" />
+
+      <ImageGallery />
+
+      {/* <FAQs results={selectedResults} /> */}
     </Container>
   );
 }
---
+++ src/app/components/card/mini-card.component.tsx
@@ -0,0 +1,201 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+import { CardIcon } from "./card-icon.component";
+import { CardDetails } from "./card-details.component";
+import Link from "next/link";
+import { IconSchedule } from "../icons/icon-schedule.component";
+
+type Props = {
+  header: string;
+  label?: string;
+  openDate?: string;
+  startDate?: string;
+  width?: string;
+};
+
+type containerProps = {
+  width?: string;
+};
+
+const Container = styled.div<containerProps>`
+  background: var(--off-black);
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin: 0 auto;
+  position: relative;
+  width: ${({ width }) => (width ? width : "100vw")};
+
+  @media (min-width: 800px) {
+    flex-direction: row;
+  }
+`;
+
+const MiniCard = styled.div`
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  position: relative;
+  width: 25%;
+
+  &:hover {
+    box-shadow: 0 0 0 2px var(--blue);
+  }
+`;
+
+const Label = styled.p`
+  color: var(--blue);
+  font-size: 12px;
+  font-weight: bold;
+  line-height: 1.5;
+`;
+
+const Header = styled.p`
+  color: var(--white);
+  font-size: 28px;
+  font-weight: bold;
+  line-height: 1.5;
+`;
+
+const Text = styled.p`
+  color: var(--white);
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px 0;
+`;
+
+const BoldText = styled.span`
+  color: var(--white);
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 1.5;
+`;
+
+interface rowProps {
+  background?: string;
+}
+const Row = styled.div<rowProps>`
+  background: ${({ background }) => background};
+  display: flex;
+  flex-direction: row;
+  padding: 25px 25px;
+  position: relative;
+  width: 100%;
+`;
+
+interface colProps {
+  width?: string;
+}
+const Col = styled.div<colProps>`
+  width: ${({ width }) => (width ? width : "50%")};
+`;
+
+const Icon = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: right;
+  width: 50%;
+`;
+
+export const MiniCards = ({ width }: Props) => {
+  return (
+    <Container width={width}>
+      <MiniCard>
+        <Row>
+          <Col>
+            <Label>Next up!</Label>
+            <Header>Spring 2024</Header>
+          </Col>
+          <Icon>
+            <IconSchedule height="48" width="48" />
+          </Icon>
+        </Row>
+
+        <Row background="var(--medium-grey)">
+          <Col width="100%">
+            <Text>
+              <BoldText>Opens</BoldText>: November 6, 2023
+            </Text>
+            <Text>
+              <BoldText>Starts</BoldText>: January 22, 2024
+            </Text>
+          </Col>
+        </Row>
+      </MiniCard>
+
+      <MiniCard>
+        <Row>
+          <Col>
+            <Label>Next up!</Label>
+            <Header>Spring 2024</Header>
+          </Col>
+          <Icon>
+            <IconSchedule height="48" width="48" />
+          </Icon>
+        </Row>
+
+        <Row background="var(--medium-grey)">
+          <Col width="100%">
+            <Text>
+              <BoldText>Opens</BoldText>: November 6, 2023
+            </Text>
+            <Text>
+              <BoldText>Starts</BoldText>: January 22, 2024
+            </Text>
+          </Col>
+        </Row>
+      </MiniCard>
+
+      <MiniCard>
+        <Row>
+          <Col>
+            <Label>Next up!</Label>
+            <Header>Spring 2024</Header>
+          </Col>
+          <Icon>
+            <IconSchedule height="48" width="48" />
+          </Icon>
+        </Row>
+
+        <Row background="var(--medium-grey)">
+          <Col width="100%">
+            <Text>
+              <BoldText>Opens</BoldText>: November 6, 2023
+            </Text>
+            <Text>
+              <BoldText>Starts</BoldText>: January 22, 2024
+            </Text>
+          </Col>
+        </Row>
+      </MiniCard>
+
+      <MiniCard>
+        <Row>
+          <Col>
+            <Label>Next up!</Label>
+            <Header>Spring 2024</Header>
+          </Col>
+          <Icon>
+            <IconSchedule height="48" width="48" />
+          </Icon>
+        </Row>
+
+        <Row background="var(--medium-grey)">
+          <Col width="100%">
+            <Text>
+              <BoldText>Opens</BoldText>: November 6, 2023
+            </Text>
+            <Text>
+              <BoldText>Starts</BoldText>: January 22, 2024
+            </Text>
+          </Col>
+        </Row>
+      </MiniCard>
+    </Container>
+  );
+};
