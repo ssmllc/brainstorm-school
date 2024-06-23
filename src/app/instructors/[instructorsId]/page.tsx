9e684cb23bdfc9a21489a379b3727aee9a1af86d
MACC <cesar@studiosupermassive.com>
2024-06-22 23:27:57 -0700

    Add courses by this instructor

--- src/app/instructors/[instructorsId]/page.tsx
+++ src/app/instructors/[instructorsId]/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 
 const fetchData = async () => {
   const query =
-    "https://y8rjsgga.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27instructors%27%5D+%7B%0A++_id%2C%0A++title%2C%0A++%22headshot%22%3A+photo.asset-%3E+url%2C%0A++%22slug%22%3A+slug.current%2C%0A++profession%2C%0A++%22imageUrl%22%3A+poster.asset-%3Eurl%2C%0A++bio%2C%0A++gallery+%5B%5D+-%3E+%7B%0A++++%22poster%22%3A+portfolio.asset-%3Eurl%0A++%7D%0A%7D";
+    "https://y8rjsgga.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27instructors%27%5D+%7B%0A++_id%2C%0A++title%2C%0A++%22headshot%22%3A+photo.asset-%3E+url%2C%0A++%22slug%22%3A+slug.current%2C%0A++profession%2C%0A++%22imageUrl%22%3A+poster.asset-%3Eurl%2C%0A++bio%2C%0A++gallery+%5B%5D+-%3E+%7B%0A++++%22poster%22%3A+portfolio.asset-%3Eurl%0A++%7D%2C%0A++courses%5B%5D+-%3E+%7B%0A++++_id%2C%0A++++%22slug%22%3A+slug.current%2C%0A++++%22imageUrl%22%3A+preview.asset-%3Eurl%2C%0A++++code%2C%0A++++category%2C%0A++++section%2C%0A++++name%2C%0A++++price%2C%0A++++difficulty%2C%0A++++schedule%5B%5D+-%3E%2C%0A++%7D%0A%7D";
   // const response = await fetch(query);
   const response = await fetch(query, { cache: "no-store" });
 
@@ -39,7 +39,8 @@ export default async function Instructors({
     return instructor.slug === params.instructorsId;
   });
 
-  const { title, profession, headshot, imageUrl, bio } = selectedInstructor[0];
+  const { title, profession, headshot, imageUrl, bio, courses } =
+    selectedInstructor[0];
 
   console.log("title", title);
   return (
@@ -57,7 +58,7 @@ export default async function Instructors({
       <FlexBox
         flexdirection="column"
         sm_margin="50px auto"
-        margin="100px auto"
+        margin="100px auto 50px"
         xl_margin="100px auto 50px"
       >
         <FlexBox
@@ -119,39 +120,61 @@ export default async function Instructors({
           width="100%"
           xl_width="100%"
         >
-          <CardImageDetail
-            slug="workshops"
-            path="introduction-to-zbrush"
-            preview="/banner/banner-21.jpg"
-            courseTrack="2D - 3D Concept Design"
-            courseName="Introduction to ZBrush"
-            courseCode="ZB1"
-            courseTime="Fri 10am-1pm (PST)"
-            courseDuration="10-week Course"
-            width="33%"
-          />
-          <CardImageDetail
-            slug="workshops"
-            path="introduction-to-zbrush"
-            preview="/banner/banner-21.jpg"
-            courseTrack="2D - 3D Concept Design"
-            courseName="Introduction to ZBrush"
-            courseCode="ZB1"
-            courseTime="Fri 10am-1pm (PST)"
-            courseDuration="10-week Course"
-            width="33%"
-          />
-          <CardImageDetail
-            slug="workshops"
-            path="introduction-to-zbrush"
-            preview="/banner/banner-21.jpg"
-            courseTrack="2D - 3D Concept Design"
-            courseName="Introduction to ZBrush"
-            courseCode="ZB1"
-            courseTime="Fri 10am-1pm (PST)"
-            courseDuration="10-week Course"
-            width="33%"
-          />
+          {courses &&
+            courses.map((course: any) => {
+              const {
+                _id,
+                imageUrl,
+                slug,
+                category,
+                name,
+                code,
+                schedule,
+                price,
+                difficulty,
+              } = course;
+              return (
+                // <CardImageDetail
+                //   key={course._id}
+                //   base={course.slug}
+                //   preview={course.imageUrl}
+                //   slug={course.slug}
+                //   path={course.slug}
+                //   courseTrack={course.category}
+                //   courseName={course.name && course.name}
+                //   courseCode={course.code && course.code}
+                //   courseTime={course?.schedule && course?.schedule[0]?.time}
+                //   courseDuration={
+                //     course?.schedule && course?.schedule[0]?.duration
+                //   }
+                //   width="25%"
+                // />
+
+                <CardImageDetail
+                  key={_id}
+                  base="/courses"
+                  preview={imageUrl}
+                  slug={
+                    category
+                      ? category
+                          .toLocaleLowerCase()
+                          .replaceAll(" ", "-")
+                          .replaceAll("&", "and")
+                      : "foundation"
+                  }
+                  path={slug}
+                  courseTrack={category}
+                  courseName={name}
+                  courseCode={code}
+                  courseTime={schedule && schedule[0]?.time}
+                  courseDuration={schedule && schedule[0]?.duration}
+                  coursePrice={price}
+                  courseDifficulty={difficulty}
+                  width={"25%"}
+                />
+              );
+            })}
+
           {/* <CardImageDetail
               slug="workshops"
               path="introduction-to-zbrush"
