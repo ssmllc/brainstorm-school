cadaf62cde4a4e38cc2dfc64240fece0efa6ce26
MACC <cesar@studiosupermassive.com>
2024-07-01 00:21:50 -0700

    clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.scss";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { BrainstormProvider } from "./context/context-provider";
 import StyledComponentsRegistry from "../../lib/registry";
 import Navigation from "./components/navigation/navigation.component";
@@ -8,7 +7,6 @@ import Footer from "./components/footer/footer.component";
 import { TracksProvider } from "./context/tracks-context-provider";
 import Head from "next/head";
 import { GoogleTagManager } from "@next/third-parties/google";
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Brainstorm School",
